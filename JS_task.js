d2f584bed460e4b18d861d288cff95a10040d114
tcee42 <toochukwuali@gmail.com>
2020-03-25 07:18:30 +0000

    My first JS task

---
+++ JS_task.js
@@ -0,0 +1,17 @@
+const name = "Tochukwu"
+
+const courses = ["Javascript", "HTML", "CSS"]
+
+console.log("Name: " + name)
+console.log("Courses: " + courses)
+
+for (let index = 1; index <= 200; index++) {
+    if (index % 2 !== 0) {
+        console.log(index)
+    } else {
+        null
+    }
+}
+
+
+
